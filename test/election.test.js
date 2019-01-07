ed3c2c8e887fb94fdd0c31ea807fd77d738ae8bd
bevrivling <bev.rivling@gmail.com>
2019-01-07 17:21:50 +0000

    stripped pet shop boilerplate

--- test/election.test.js
+++ test/election.test.js
@@ -10,4 +10,23 @@ contract("Election", accounts => {
         assert.equal(count, 2);
       });
   });
+
+  it("it initiallises the candidates with the correct values", () => {
+    return Election.deployed()
+      .then(inst => {
+        electionInstance = inst;
+        return electionInstance.candidates(1);
+      })
+      .then(candidate => {
+        assert.equal(candidate[0], 1, "contains the correct id");
+        assert.equal(candidate[1], "Big John", "contains the correct name");
+        assert.equal(candidate[2], 0, "contains the correct votes count");
+        return electionInstance.candidates(2);
+      })
+      .then(candidate => {
+        assert.equal(candidate[0], 2, "contains the correct id");
+        assert.equal(candidate[1], "Little Susan", "contains the correct name");
+        assert.equal(candidate[2], 0, "contains the correct votes count");
+      });
+  });
 });
